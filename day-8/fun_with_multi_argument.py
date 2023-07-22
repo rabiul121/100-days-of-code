afc8f0650e353927e053f70886603a0889a434e0
Md. Rabiul Islam <91551048+rabiul121@users.noreply.github.com>
2023-07-23 00:30:16 +0600

    added day-8 codes

---
+++ day-8/fun_with_multi_argument.py
@@ -0,0 +1,8 @@
+# function with multiple argument
+
+def greet(name, location):
+    print(f"Hello, {name}.")
+    print(f"How is the weather in {location} today?")
+
+
+ greet('Robi', 'Jessore')
---
+++ day-8/function_with-input.py
@@ -0,0 +1,7 @@
+def greet_with_name(name):
+    print(f"Hello, {name}")
+    print(f"How do you do, {name}")
+    print(f"Isn't the weather great today, {name}")
+
+
+greet_with_name("Robi")
