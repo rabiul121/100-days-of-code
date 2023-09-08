8717d5ec74de83e82d131b964bedcdff39a44fdd
Md. Rabiul Islam <91551048+rabiul121@users.noreply.github.com>
2023-09-08 11:43:52 +0600

    updated number guessing game, added v2 of the code

--- day-12/12.2 number_guessing_game.py
+++ day-12/12.2 number_guessing_game.py
@@ -1,44 +1,49 @@
-# Guess a number
+# Guess a number game
 import random
-from art import logo
 
-print(logo)
+from art import logo
 answer = int(random.randint(0, 100))
 print("Welcome to the Number Guessing Game!")
 print("I am thinking of a number between 1 and 100.")
-# print(f"Pssst, the correct answer is {answer}")
 
-level = input("Chose a difficulty level, type 'easy' or 'hard': ")
-if level == 'easy':
-    attempt = 10
-elif level == 'hard':
-    attempt = 5
-else:
-    print("Wrong input! Try again.")
+print(logo)
+
+
+def set_level():
+    level = input("Chose a difficulty level, type 'easy' or 'hard': ")
+    if level == 'easy':
+        return 10
+    elif level == 'hard':
+        return 5
+
+
+attempt = set_level()
+
+
+def check_remaining_attempts():
+    if attempt > 1:
+        print("Guess again!")
+    else:
+        print("You have run out of guess, you lose!")
 
 
 def game():
-    remaining_attempt = attempt - 1
+    global attempt
     for _ in range(attempt):
+        print(f"You have {attempt} attempts remaining to guess!")
         guess = int(input("Make a guess: "))
         if guess == answer:
             print(f"You got it! The answer was {answer}.")
-            break
+            return
         elif guess < answer:
             print("Too low.")
-            print("Guess again!")
-            if remaining_attempt == 0:
-                print("You have run out of guess, you lose!")
-            else:
-                print(f"You have {remaining_attempt} attempts remaining to guess!")
-        else:
-            print("Too high.")
-            if remaining_attempt == 0:
-                print("You have run out of guess, you lose!")
-            else:
-                print(f"You have {remaining_attempt} attempts remaining to guess!")
+            check_remaining_attempts()
 
-        remaining_attempt -= 1
+        elif guess > answer:
+            print("Too high.")
+            check_remaining_attempts()
+
+        attempt -= 1
 
 
 game()
