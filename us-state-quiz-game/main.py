7a1753622239d816e7ddf177af0e54b713edc26b
Md. Rabiul Islam <91551048+rabiul121@users.noreply.github.com>
2024-12-23 07:34:07 +0600

    Day-25 completed

---
+++ us-state-quiz-game/main.py
@@ -0,0 +1,31 @@
+import turtle
+import pandas as pd
+
+screen = turtle.Screen()
+screen.title("U.S. State Quiz Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+guessed_states = []
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?").title()
+    data = pd.read_csv("50_states.csv")
+    all_states = data["state"].to_list()
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pd.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(state_data.x.item(), state_data.y.item())
+        t.write(answer_state)
